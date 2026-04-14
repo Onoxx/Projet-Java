9c95223a81cd04c067510ca72a61a96ac41cde6e
Noa <noaforseille29@gmail.com>
2026-04-14 14:55:08 +0200

    Creation MainWindow + upload resources

---
+++ MainWindow.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainWindow extends JFrame {
+    private Container mainContainer;
+    public MainWindow() {
+        super("NexaPC");
+        setBounds(0, 0, 1280, 720);
+
+        addWindowListener (new WindowAdapter() {
+            public void windowClosing (WindowEvent e) {
+                System.exit(0);
+            }
+        } );
+
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/res/icon.png")));
+
+        mainContainer = this.getContentPane();
+        mainContainer.setLayout(new BorderLayout());
+        this.setLocationRelativeTo(null);
+
+        mainContainer.add(new JLabel("Bienvenue chez NexaPC, le constructeur pc de vos streamers préférés!", SwingConstants.CENTER), BorderLayout.NORTH);
+
+        setVisible(true);
+    }
+}
