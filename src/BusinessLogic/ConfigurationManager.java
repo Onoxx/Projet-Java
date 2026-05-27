2539ea8a9bbca90d10807449f81b2efa367e12d1
Onoxx <leo.clarinval@icloud.com>
2026-05-27 14:29:20 +0200

    Fix-ConfigurationManager

--- src/BusinessLogic/ConfigurationManager.java
+++ src/BusinessLogic/ConfigurationManager.java
@@ -2,16 +2,12 @@ package BusinessLogic;
 
 import DAO.Configuration.ConfigurationDAO;
 import DAO.Configuration.ConfigurationDAOImpl;
-import Exceptions.IncompatibleComponentException;
-import Exceptions.InvalidQuantityException;
 import MVC.Model.*;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Objects;
 
-public class ConfigurationManager implements ConfigurationDAO {
+public class ConfigurationManager {
     private ConfigurationDAO configurationDAO;
 
     public ConfigurationManager() {
@@ -31,60 +27,31 @@ public class ConfigurationManager implements ConfigurationDAO {
         return configurationDAO.searchConfigByUserDateRGB(user, age, hasRGB);
     }
 
-    @Override
     public ArrayList<StorageCoolingSearch> searchConfigWithStorageCooling(String storageName, String coolingName, String motherBoard) {
         return configurationDAO.searchConfigWithStorageCooling(storageName, coolingName, motherBoard);
     }
+    public double computeTotalPrice(Configuration config) {
+        double totalPrice = 0;
 
-    public void verifyConfiguration(Configuration configuration, HashMap<Storage, Integer> storages, HashMap<Cooling, Integer> coolings) {
-        Processor currentProcessor = configuration.getProcessor();
-        GraphicCard currentGraphicCard = configuration.getGraphicCard();
-        Case computerCase = configuration.getComputerCase();
-        MotherBoard currentMotherBoard = configuration.getMotherBoard();
-        Ram currentRam = configuration.getRam();
-        if(currentProcessor != null && currentMotherBoard != null
-                && !Objects.equals(currentProcessor.getSocket(), currentMotherBoard.getSocket())){
-            throw new IncompatibleComponentException("socket","processeur","carte mère");
+        if (config.getProcessor() != null)    totalPrice += config.getProcessor().getPrice();
+        if (config.getMotherBoard() != null)  totalPrice += config.getMotherBoard().getPrice();
+        if (config.getRam() != null)          totalPrice += config.getRam().getPrice();
+        if (config.getComputerCase() != null) totalPrice += config.getComputerCase().getPrice();
+        if (config.getGraphicCard() != null)  totalPrice += config.getGraphicCard().getPrice();
+
+        for (HashMap.Entry<Storage, Integer> entry : config.getStorages().entrySet()) {
+            totalPrice += entry.getKey().getPrice() * entry.getValue();
         }
-        if(currentGraphicCard != null && computerCase != null
-                && currentGraphicCard.getLength() > computerCase.getMaxLengthGPU()){
-            throw new IncompatibleComponentException("longueur","carte graphique","boitier");
+        for (HashMap.Entry<Cooling, Integer> entry : config.getCoolings().entrySet()) {
+            totalPrice += entry.getKey().getPrice() * entry.getValue();
         }
-        if(computerCase != null && currentMotherBoard != null
-                && !Objects.equals(computerCase.getFomat(), currentMotherBoard.getFormat())){
-            throw new IncompatibleComponentException("fomat","boitier","carte mère");
-        }
-        if(currentMotherBoard != null && currentRam != null
-                && !Objects.equals(currentMotherBoard.getRamType(), currentRam.getType())){
-            throw new IncompatibleComponentException("type de ram","carte mère","ram");
-        }
-        for(HashMap.Entry<Cooling, Integer> entry : coolings.entrySet()) {
-            Cooling cooling = entry.getKey();
-            int quantity = entry.getValue();
-            if(quantity < 1){
-                throw new InvalidQuantityException(quantity);
-            }
-            if(currentProcessor != null &&
-                    !Objects.equals(
-                            cooling.getComptabileSocket(),
-                            currentProcessor.getSocket())){
-                throw new IncompatibleComponentException(
-                        "socket",
-                        "refroidissement",
-                        "processeur"
-                );
-            }
-            if(computerCase != null &&
-                    cooling.getHeight() > computerCase.getMaxHeightVentirad()){
-                throw new IncompatibleComponentException("taille","refroidissement","boitier");
-            }
-        }
-        for(HashMap.Entry<Storage, Integer> entry : storages.entrySet()) {
-            int quantity = entry.getValue();
-            if(quantity < 1){
-                throw new InvalidQuantityException(quantity);
-            }
-        }
+
+        return totalPrice;
     }
+
+    public ArrayList<Configuration> getConfigurations() {
+        return configurationDAO.getConfigurations();
+    }
+
 }
 
